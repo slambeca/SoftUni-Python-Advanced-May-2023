0d4f2332b0032c13ed78d3a74c38c66213b78ea0
Borislav Ivanov <95913250+slambeca@users.noreply.github.com>
2023-05-10 20:32:11 +0300

    Update stacked_queries.py

--- 1.2. Lists as Stacks and Queues - Exercise/stacked_queries.py
+++ 1.2. Lists as Stacks and Queues - Exercise/stacked_queries.py
@@ -29,28 +29,71 @@ stack.reverse()
 
 print(*stack, sep=", ")
 
-# 100/100 in Judge
-
 # Variant 2 with map_functions and lambda
+# from collections import deque
 
-from collections import deque
+# numbers = deque()
 
-numbers = deque()
+# map_functions = {
+#     1: lambda x: numbers.append(x[1]),
+#     2: lambda x: numbers.pop() if numbers else None,
+#     3: lambda x: print(max(numbers)) if numbers else None,
+#     4: lambda x: print(min(numbers)) if numbers else None,
+# }
 
-map_functions = {
-    1: lambda x: numbers.append(x[1]),
-    2: lambda x: numbers.pop() if numbers else None,
-    3: lambda x: print(max(numbers)) if numbers else None,
-    4: lambda x: print(min(numbers)) if numbers else None
-}
+# for _ in range(int(input())):
+#     number_data = [int(x) for x in input().split()]
 
-for _ in range(int(input())):
-    number_data = [int(x) for x in input().split()]
+#     map_functions[number_data[0]](number_data)
 
-    map_functions[number_data[0]](number_data)
+# numbers.reverse()
 
-numbers.reverse()
+# print(*numbers, sep=", ")
 
-print(*numbers, sep=", ")
+# # Variant 3
+# from collections import deque
 
-# 100/100 in Judge
+# numbers = deque()
+
+# for _ in range(int(input())):
+#     number_data = [int(x) for x in input().split()]
+#     # number_data = map(int, input().split())
+#     command = number_data[0]
+
+#     if command == 1:
+#         numbers.append(number_data[1])
+#     elif command == 2:
+#         if numbers:
+#             numbers.pop()
+#     elif command == 3:
+#         if numbers:
+#             print(max(numbers))
+#     elif command == 4:
+#         if numbers:
+#             print(min(numbers))
+
+# numbers.reverse()
+
+# print(*numbers, sep=", ")
+
+# # Variant 4, same as Variant 2
+# from collections import deque
+
+# numbers = deque()
+
+# map_functions = {
+#     1: lambda x: numbers.append(x[1]),    # x == number_data => 1, 97
+#     2: lambda x: numbers.pop() if numbers else None,    # ternary operator
+#     3: lambda x: print(max(numbers)) if numbers else None,
+#     4: lambda x: print(min(numbers)) if numbers else None,
+# }
+
+# for _ in range(int(input())):
+#     number_data = [int(x) for x in input().split()]
+#     map_functions[number_data[0]](number_data)
+#     # func = map_functions[number_data[0]]
+#     # func(number_data)
+
+# numbers.reverse()
+
+# print(*numbers, sep=", ")
