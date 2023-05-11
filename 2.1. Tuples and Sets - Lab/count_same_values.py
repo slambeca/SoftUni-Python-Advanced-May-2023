a91cc9a7026f5ad521d2ce9539c9e4d308a889ec
Borislav Ivanov <slambeca@abv.bg>
2023-05-11 18:52:12 +0300

    Added additional solution

--- 2.1. Tuples and Sets - Lab/count_same_values.py
+++ 2.1. Tuples and Sets - Lab/count_same_values.py
@@ -1,4 +1,4 @@
-numbers = [float(x) for x in input().split()]    # [-2.5, 4.0, 3.0, -2.5, -5.5, 4.0, 3.0, 3.0, -2.5, 3.0]
+numbers = [float(x) for x in input().split()]    
 
 dictionary = {}
 
@@ -11,16 +11,16 @@ for number in numbers:
 for key, values in dictionary.items():
     print(f"{key} - {len(values) + 1} times")
 
-# # Variant 2
-# values = tuple(map(float, input().split()))  # (-2.5, 4.0, 3.0, -2.5, -5.5, 4.0, 3.0, 3.0, -2.5, 3.0)
+# Variant 2
+# values = tuple(map(float, input().split())) 
 #
 # counter_of_values = {value: values.count(value) for value in values}
 #
 # for key, value in counter_of_values.items():
 #     print(f"{key} - {value} times")
 #
-# # Variant 3
-# values = tuple(map(float, input().split()))  # (-2.5, 4.0, 3.0, -2.5, -5.5, 4.0, 3.0, 3.0, -2.5, 3.0)
+# Variant 3
+# values = tuple(map(float, input().split()))  
 #
 # values_counter = {}
 #
@@ -33,3 +33,16 @@ for key, values in dictionary.items():
 #     print(f"{key} - {value} times")
 #
 # # [print(f"{key} - {value} times") for key, value in counter_of_values.items()]
+
+# Variant 4
+# numbers = tuple(input().split())
+#
+# occurrences = {}
+#
+# for el in numbers:
+#     if el not in occurrences.keys():
+#         occurrences[el] = 0
+#     occurrences[el] += 1
+#
+# for key, value in occurrences.items():
+#     print(f"{float(key)} - {value} times")
