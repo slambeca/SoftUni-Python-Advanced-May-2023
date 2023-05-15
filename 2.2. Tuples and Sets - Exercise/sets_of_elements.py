14090001fcccae0e2d22c56d4458db60d51cc378
Borislav Ivanov <slambeca@abv.bg>
2023-05-15 11:49:59 +0300

    Additional solution added, without the use of the intersection method.

--- 2.2. Tuples and Sets - Exercise/sets_of_elements.py
+++ 2.2. Tuples and Sets - Exercise/sets_of_elements.py
@@ -28,3 +28,11 @@ print(*set_one.intersection(set_two), sep="\n")
 # second_set = {int(input()) for _ in range(m)}
 #
 # print(*first_set.intersection(second_set), sep="\n")
+
+# Variant 4
+# n, m = [int(x) for x in input().split()]
+
+# first_set = {int(input()) for _ in range(n)}
+# second_set = {int(input()) for _ in range(m)}
+
+# print(*(first_set & second_set), sep="\n")
