033ad8e79391e282d7595fba1a8c6843ec93faa4
Borislav Ivanov <slambeca@abv.bg>
2023-05-15 14:52:20 +0300

    Another solution added.

--- 2.2. Tuples and Sets - Exercise/longest_intersection.py
+++ 2.2. Tuples and Sets - Exercise/longest_intersection.py
@@ -38,3 +38,19 @@ print(f"Longest intersection is {longest_intersection} with length {len(longest_
 # print(f"Longest intersection is "
 #       f"[{', '.join([str(x) for x in longest_intersection])}] "
 #       f"with length {len(longest_intersection)}")
+
+# Variant 3, similar to Variant 2
+# longest_intersection = set()
+#
+# for _ in range(int(input())):
+#     first_data, second_data = [element.split(",") for element in input().split("-")]
+#
+#     first_range = set(range(int(first_data[0]), int(first_data[1]) + 1))
+#     second_range = set(range(int(second_data[0]), int(second_data[1]) + 1))
+#
+#     intersection = first_range.intersection(second_range)
+#
+#     if len(intersection) > len(longest_intersection):
+#         longest_intersection = intersection
+#
+# print(f"Longest intersection is {list(longest_intersection)} with length {len(longest_intersection)}")
