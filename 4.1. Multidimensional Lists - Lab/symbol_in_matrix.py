4d78ebf07af7a46808470b809ba5ee296b2bd3a4
Borislav Ivanov <slambeca@abv.bg>
2023-05-18 17:44:35 +0300

    Additional solution added

--- 4.1. Multidimensional Lists - Lab/symbol_in_matrix.py
+++ 4.1. Multidimensional Lists - Lab/symbol_in_matrix.py
@@ -29,3 +29,26 @@ matrix = read_matrix()    # [['A', 'B', 'C'], ['D', 'E', 'F'], ['X', '!', '@']]
 special_symbol = input()
 
 print_function(check_for_special_symbol(matrix, special_symbol), special_symbol)
+
+# Variant 2
+# rows = int(input())
+# 
+# matrix = []
+# 
+# is_found = False
+# 
+# for _ in range(rows):
+#     current_row = list(input())
+#     matrix.append(current_row)
+# 
+# special_symbol = input()
+# 
+# for row in range(rows):
+#     for col in range(rows):
+#         if matrix[row][col] == special_symbol:
+#             print((row, col))
+#             is_found = True
+#             raise SystemExit
+# 
+# if not is_found:
+#     print(f"{special_symbol} does not occur in the matrix")
