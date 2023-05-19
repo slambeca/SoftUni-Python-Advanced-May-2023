c09f3e5ae31feb621c25cef0d4823e3132309d3f
Borislav Ivanov <slambeca@abv.bg>
2023-05-19 16:23:06 +0300

    Additional solution added

--- 4.2. Multidimensional Lists - Exercise/matrix_of_palindromes.py
+++ 4.2. Multidimensional Lists - Exercise/matrix_of_palindromes.py
@@ -6,13 +6,24 @@ for row in range(start_end, start_end + rows):
     for column in range(start_end, start_end + columns):
         print(f"{chr(row)}{chr((row + column) - start_end)}{chr(row)}", end=" ")
     print()
-    
+
 # Variant 2
 # rows, cols = [int(x) for x in input().split()]
-# 
+#
 # start = ord("a")
-# 
+#
 # for row in range(start, start + rows):
 #     for col in range(start, start + cols):
 #         print(f"{chr(row)}{chr(col + abs(97 - row))}{chr(row)}", end=" ")
+#     print()
+
+# Variant 3
+# rows, cols = [int(x) for x in input().split()]
+#
+# start = ord("a")
+#
+# for row in range(start, start + rows):
+#     for col in range(start, start + cols):
+#         print(f"{chr(row)}{chr(col + (row - 97))}{chr(row)}", end=" ")
+#
 #     print()
