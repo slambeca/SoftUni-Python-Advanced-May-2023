666294a2c05042eabf79259e1533058bace812a7
Borislav Ivanov <slambeca@abv.bg>
2023-05-21 14:29:08 +0300

    Second solution added with list comprehensions

--- 4.3. Multidimensional Lists - Exercise/flatten_lists.py
+++ 4.3. Multidimensional Lists - Exercise/flatten_lists.py
@@ -6,3 +6,7 @@ for substring in line[::-1]:
     matrix.extend(substring.split())    # We remove all the spaces
 
 print(*matrix)
+
+# Variant 2
+# numbers = [string.split() for string in input().split("|")]
+# print(*[' '.join(sub_list) for sub_list in numbers[::-1] if sub_list])
