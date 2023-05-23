b1e9e91c22dd61f5b57bda42602b52601719255d
Borislav Ivanov <slambeca@abv.bg>
2023-05-23 13:34:13 +0300

    Second solution added (functools - reduce)

--- 5.1. Functions Advanced - Lab/operate.py
+++ 5.1. Functions Advanced - Lab/operate.py
@@ -13,3 +13,21 @@ def operate(operator, *args):
             result /= num
 
     return result
+
+# Variant 2
+# from functools import reduce
+# 
+# 
+# def operate(operator, *args):
+#     if operator == "+":
+#         return reduce(lambda x, y: x + y, args)
+#     elif operator == "-":
+#         return reduce(lambda x, y: x - y, args)
+#     elif operator == "*":
+#         return reduce(lambda x, y: x * y, args)
+#     elif operator == "/":
+#         return reduce(lambda x, y: x / y, args)
+# 
+# 
+# print(operate("+", 1, 2, 3))
+# print(operate("*", 3, 4))
