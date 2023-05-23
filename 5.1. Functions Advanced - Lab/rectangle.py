4d8444c0d0dc6294488308a9b124feead0f4e81f
Borislav Ivanov <slambeca@abv.bg>
2023-05-23 12:56:22 +0300

    Second solution added

--- 5.1. Functions Advanced - Lab/rectangle.py
+++ 5.1. Functions Advanced - Lab/rectangle.py
@@ -14,3 +14,22 @@ Rectangle perimeter: {perimeter()}"""
 
 print(rectangle(2, 10))
 print(rectangle('2', 10))
+
+# Variant 2
+
+
+# def rectangle(lenth, width):
+#     if not isinstance(lenth, int) or not isinstance(width, int):
+#         return "Enter valid values!"
+# 
+#     def area():
+#         return lenth * width
+# 
+#     def perimeter():
+#         return (lenth * 2) + (width * 2)
+# 
+#     return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
+# 
+# 
+# print(rectangle(2, 10))
+# print(rectangle('2', 10))
