61ef6fb1512566b7960d8d91f9a107f5c68d8627
Borislav Ivanov <slambeca@abv.bg>
2023-05-26 15:51:59 +0300

    Second solution added

--- 5.2. Functions Advanced - Exercise/concatenate.py
+++ 5.2. Functions Advanced - Exercise/concatenate.py
@@ -9,3 +9,18 @@ def concatenate(*args, **kwargs):
 
 print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
 print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
+
+# Variant 2
+#
+#
+# def concatenate(*args, **kwargs):
+#     final_string = "".join(args)
+# 
+#     for key, value in kwargs.items():
+#         final_string = final_string.replace(key, value)
+# 
+#     return final_string
+# 
+# 
+# print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
+# print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
