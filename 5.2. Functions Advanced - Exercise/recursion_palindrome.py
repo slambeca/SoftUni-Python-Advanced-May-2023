6d1246e5cbd899a8f338cfa6356f932d49975f62
Borislav Ivanov <slambeca@abv.bg>
2023-05-29 15:01:57 +0300

    Third solution added

--- 5.2. Functions Advanced - Exercise/recursion_palindrome.py
+++ 5.2. Functions Advanced - Exercise/recursion_palindrome.py
@@ -22,3 +22,18 @@ print(palindrome("peter", 0))
 #
 # print(palindrome("abcba", 0))
 # print(palindrome("peter", 0))
+
+# Variant 3
+
+
+# def palindrome(word, idx):
+#     if idx == len(word) // 2:
+#         return f"{word} is a palindrome"
+# 
+#     if word[idx] != word[-idx-1]:
+#         return f"{word} is not a palindrome"
+# 
+#     return palindrome(word, idx + 1)
+# 
+# 
+# print(palindrome("abcra", 0))
