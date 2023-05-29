e8da1760d7bf8689c1b793fc3ed2e4ee62636ce1
Borislav Ivanov <slambeca@abv.bg>
2023-05-29 16:11:52 +0300

    Second solution added

--- 5.2. Functions Advanced - Exercise/fill_the_box.py
+++ 5.2. Functions Advanced - Exercise/fill_the_box.py
@@ -22,3 +22,28 @@ def fill_the_box(*args):
 print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
 print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
 print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))
+
+# Variant 2
+# def fill_the_box(height, length, width, *data):
+#     space = height * length * width
+#     cubes_left = 0
+
+#     for item in data:
+#         if item == "Finish":
+#             break
+#         left_from_cube = item - space
+#         space -= item
+
+#         if space < 0:
+#             cubes_left += left_from_cube
+#             space = 0
+
+#     if space:
+#         return f"There is free space in the box. You could put {space} more cubes."
+
+#     return f"No more free space! You have {cubes_left} more cubes."
+
+
+# print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
+# print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
+# print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30,))
