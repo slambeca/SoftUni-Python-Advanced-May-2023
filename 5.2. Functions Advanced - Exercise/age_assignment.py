f9862ed9310bccd621488f92da619f0ccd533c8c
Borislav Ivanov <slambeca@abv.bg>
2023-05-29 14:38:30 +0300

    Second solution added

--- 5.2. Functions Advanced - Exercise/age_assignment.py
+++ 5.2. Functions Advanced - Exercise/age_assignment.py
@@ -15,3 +15,20 @@ def age_assignment(*names, **data):
 
 
 print(age_assignment("Peter", "George", G=26, P=19))
+
+# Variant 2
+#
+#
+# def age_assignment(*names, **data):
+#     result = []
+#     for letter, age in data.items():
+#         for name in names:
+#             if name.startswith(letter):
+#                 result.append(f"{name} is {age} years old.")
+#                 break
+#
+#     return '\n'.join(sorted(result))
+#
+#
+# print(age_assignment("Peter", "George", G=26, P=19))
+# print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
