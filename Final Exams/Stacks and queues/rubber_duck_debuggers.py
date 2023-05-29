91a7c5c4aa3ce914372ad2a0abef53e05fe65bca
Borislav Ivanov <slambeca@abv.bg>
2023-05-29 13:17:44 +0300

    Add files via upload

---
+++ Final Exams/Stacks and queues/rubber_duck_debuggers.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+time_per_task = deque([int(x) for x in input().split()])    # deque([10, 15, 12, 18, 22, 6])
+number_of_tasks = [int(x) for x in input().split()]    # [12, 16, 5, 6, 9, 1]
+
+dictionary = {
+    "Darth Vader Ducky": 0,
+    "Thor Ducky": 0,
+    "Big Blue Rubber Ducky": 0,
+    "Small Yellow Rubber Ducky": 0
+}
+
+while time_per_task and number_of_tasks:
+    current_time_per_task = time_per_task.popleft()    # 10
+    current_number_of_tasks = number_of_tasks.pop()    # 1
+
+    result = current_time_per_task * current_number_of_tasks    # 10
+
+    if 0 <= result <= 60:
+        dictionary["Darth Vader Ducky"] += 1
+    elif 61 <= result <= 120:
+        dictionary["Thor Ducky"] += 1
+    elif 121 <= result <= 180:
+        dictionary["Big Blue Rubber Ducky"] += 1
+    elif 181 <= result <= 240:
+        dictionary["Small Yellow Rubber Ducky"] += 1
+    elif result > 240:
+        current_number_of_tasks -= 2
+        number_of_tasks.append(current_number_of_tasks)
+        time_per_task.append(current_time_per_task)
+
+print(f"Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+for key, value in dictionary.items():
+    print(f"{key}: {value}")
