5c3f026b90d0dc46b4c008f8449fb5c7a7c43ef3
Borislav Ivanov <slambeca@abv.bg>
2023-06-01 17:11:17 +0300

    Add files via upload

---
+++ 7.2. File Handling - Exercise/directory_traversal.py
@@ -0,0 +1,30 @@
+import os
+
+current_directory = input()    # .
+
+dictionary_with_extensions = {}
+
+# print(os.listdir(current_directory))
+# ['directory_traversal.py', 'even_lines.py', 'file_manipulator.py', 'line_numbers.py', 'output.txt', 'text.txt']
+
+for file_name in os.listdir(current_directory):    # Can be either folder or a file
+    current_file = os.path.join(current_directory, file_name)    # .\directory_traversal.py
+
+    current_file = current_file.lstrip(".\\")
+
+    if os.path.isfile(current_file):
+        extension = file_name.split(".")[1]
+
+        if extension not in dictionary_with_extensions.keys():
+            dictionary_with_extensions[extension] = []
+
+        dictionary_with_extensions[extension].append(current_file)
+    elif os.path.isdir(current_directory):
+        pass
+
+sorted_dict = dict(sorted(dictionary_with_extensions.items(), key=lambda x: [x[0]]))
+
+for keys, values in sorted_dict.items():
+    print(f".{keys}")
+    for value in values:
+        print(f"- - - {value}")
