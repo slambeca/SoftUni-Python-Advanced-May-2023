366e9081773e68a6bc461e2ba3750cd82dc373e3
Borislav Ivanov <slambeca@abv.bg>
2023-06-05 20:29:34 +0300

    Update mathematical_operations.py

--- 9.1. Modules - Lab/mathematical_operations.py
+++ 9.1. Modules - Lab/mathematical_operations.py
@@ -1,3 +1,10 @@
 from custom_module_2 import mathematical_operations
 
 print(mathematical_operations(input()))
+
+# Variant 2 with eval()
+# def calculator(first_num, operator, second_num):
+#     return eval(f"{first_num}{operator}{second_num}")
+# 
+# 
+# print(calculator(2, "*", 5))
