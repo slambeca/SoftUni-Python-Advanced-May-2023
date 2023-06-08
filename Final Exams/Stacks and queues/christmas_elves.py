ddb3b30b630b5c3061421a597c0a667140189aae
Borislav Ivanov <slambeca@abv.bg>
2023-06-08 18:56:19 +0300

    Second solution added

--- Final Exams/Stacks and queues/christmas_elves.py
+++ Final Exams/Stacks and queues/christmas_elves.py
@@ -52,3 +52,52 @@ if energy:
     print(f"Elves left: {', '.join(str(x) for x in energy)}")
 if materials:
     print(f"Boxes left: {', '.join(str(x) for x in materials)}")
+    
+# # Variant 2
+# from collections import deque
+# 
+# elves = deque([int(x) for x in input().split()])
+# materials = deque([int(x) for x in input().split()])
+# 
+# total_energy = 0
+# total_toys = 0
+# iterations = 0
+# 
+# while elves and materials:
+#     elf = elves.popleft()
+#     material = materials[-1]
+# 
+#     if elf < 5:
+#         continue
+# 
+#     iterations += 1
+#     current_toys_count = 0
+# 
+#     if iterations % 3 == 0:
+#         material *= 2
+#         current_toys_count += 1
+# 
+#     if elf >= material:
+#         total_energy += material
+#         elf -= material
+# 
+#         if iterations % 5 != 0:
+#             elf += 1
+#             current_toys_count += 1
+#         else:
+#             current_toys_count = 0
+# 
+#         materials.pop()
+#     else:
+#         elf *= 2
+#         current_toys_count = 0
+# 
+#     total_toys += current_toys_count
+# 
+#     elves.append(elf)
+# 
+# print(f"Toys: {total_toys}\nEnergy: {total_energy}")
+# if elves:
+#     print(f"Elves left: {', '.join(str(x) for x in elves)}")
+# if materials:
+#     print(f"Boxes left: {', '.join(str(x) for x in materials)}")
