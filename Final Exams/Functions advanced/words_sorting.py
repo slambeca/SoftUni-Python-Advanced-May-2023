4bd65c351b5dc81da9f6c7b71aab1714599664e8
Borislav Ivanov <slambeca@abv.bg>
2023-06-09 12:18:17 +0300

    Second solution added

--- Final Exams/Functions advanced/words_sorting.py
+++ Final Exams/Functions advanced/words_sorting.py
@@ -43,3 +43,16 @@ def words_sorting(*args):
 #         'charm',
 #         'eye'
 #   ))
+
+# Variant 2
+
+
+# def words_sorting(*words):
+#     words_dict = {word: sum(map(ord, word)) for word in words}
+#
+#     total_sum = sum(words_dict.values())
+#
+#     if total_sum % 2 == 0:
+#         return '\n'.join([f"{w} - {s}" for w, s in sorted(words_dict.items(), key=lambda x: x[0])])
+#
+#     return '\n'.join([f"{w} - {s}" for w, s in sorted(words_dict.items(), key=lambda x: -x[1])])
