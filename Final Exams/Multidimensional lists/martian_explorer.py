bcef60ce35ee218e5f0675488f8abf91efca4d6f
Borislav Ivanov <slambeca@abv.bg>
2023-06-09 19:28:26 +0300

    Second solution added

--- Final Exams/Multidimensional lists/martian_explorer.py
+++ Final Exams/Multidimensional lists/martian_explorer.py
@@ -69,3 +69,46 @@ if water_collected and metal_collected and concrete_collected:
     print("Area suitable to start the colony.")
 else:
     print("Area not suitable to start the colony.")
+
+# Variant 2
+# SIZE = 6
+#
+# mars = []
+# rover_pos = []
+#
+# directions = {
+#     "up": lambda r, c: [(r - 1) % SIZE, c],
+#     "down": lambda r, c: [(r + 1) % SIZE, c],
+#     "left": lambda r, c: [r, (c - 1) % SIZE],
+#     "right": lambda r, c: [r, (c + 1) % SIZE],
+# }
+#
+# deposits = {"W": ["Water", 0], "M": ["Metal", 0], "C": ["Concrete", 0]}
+#
+# for row in range(SIZE):
+#     current_row = input().split()
+#
+#     if "E" in current_row:
+#         rover_pos = [row, current_row.index("E")]
+#
+#     mars.append(current_row)
+#
+# commands = input().split(", ")
+#
+# for command in commands:
+#     rover_pos = directions[command](*rover_pos)
+#     position = mars[rover_pos[0]][rover_pos[1]]
+#
+#     if position in deposits:
+#         print(f"{deposits[position][0]} deposit found at ({rover_pos[0]}, {rover_pos[1]})")
+#
+#         deposits[position][1] += 1
+#
+#     elif position == "R":
+#         print(f"Rover got broken at ({rover_pos[0]}, {rover_pos[1]})")
+#         break
+#
+# if all([deposits["W"][1], deposits["M"][1], deposits["C"][1]]):
+#     print("Area suitable to start the colony.")
+# else:
+#     print("Area not suitable to start the colony.")
