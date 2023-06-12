9e9c85fd5ebb8a3665b1b43528b3a431ab00b9eb
Borislav Ivanov <slambeca@abv.bg>
2023-06-12 11:35:01 +0300

    Third solution added

--- Final Exams/Stacks and queues/matching.py
+++ Final Exams/Stacks and queues/matching.py
@@ -88,3 +88,51 @@ else:
 #     print(f"Females left: {', '.join(str(x) for x in females)}")
 # else:
 #     print("Females left: none")
+
+# Variant 3
+# from collections import deque
+# 
+# males = deque([int(x) for x in input().split()])
+# females = deque([int(x) for x in input().split()])
+# 
+# matches = 0
+# 
+# while males and females:
+#     current_female = females.popleft()
+#     current_male = males.pop()
+# 
+#     if current_female <= 0:
+#         males.append(current_male)
+#         continue
+# 
+#     if current_male <= 0:
+#         females.appendleft(current_female)
+#         continue
+# 
+#     if current_male % 25 == 0:
+#         males.pop()
+#         females.appendleft(current_female)
+#         continue
+#     elif current_female % 25 == 0:
+#         females.popleft()
+#         males.append(current_male)
+#         continue
+# 
+#     if current_male == current_female:
+#         matches += 1
+#         continue
+#     else:
+#         current_male -= 2
+#         males.append(current_male)
+# 
+# print(f"Matches: {matches}")
+# if males:
+#     males.reverse()
+#     print(f"Males left: {', '.join(str(x) for x in males)}")
+# else:
+#     print(f"Males left: none")
+# 
+# if females:
+#     print(f"Females left: {', '.join(str(x) for x in females)}")
+# else:
+#     print("Females left: none")
