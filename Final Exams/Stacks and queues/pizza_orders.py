29950bcb270ee0d49c67a6e6850dd7237778cf5a
Borislav Ivanov <slambeca@abv.bg>
2023-06-12 11:13:19 +0300

    Update pizza_orders.py

--- Final Exams/Stacks and queues/pizza_orders.py
+++ Final Exams/Stacks and queues/pizza_orders.py
@@ -1,28 +1,29 @@
 from collections import deque
 
-pizza_orders = deque(map(int, input().split(", ")))
-employees = deque(map(int, input().split(", ")))
+pizza_orders = deque([int(x) for x in input().split(", ")])
+employees = deque([int(x) for x in input().split(", ")])
 
-total_pizzas = 0
+total_pizzas_made = 0
 
 while pizza_orders and employees:
     current_order = pizza_orders.popleft()
+
+    if current_order <= 0 or current_order > 10:
+        continue
+
     current_employee = employees.pop()
 
-    if current_order > 10:
-        employees.append(current_employee)
-    elif current_order <= 0:
-        employees.append(current_employee)
-    elif current_order <= current_employee:
-        total_pizzas += current_order
+    if current_order <= current_employee:
+        total_pizzas_made += current_order
+        continue
     elif current_order > current_employee:
-        remaining_value = current_order - current_employee
-        total_pizzas += current_employee
-        pizza_orders.appendleft(remaining_value)
+        total_pizzas_made += current_employee
+        pizzas_left = current_order - current_employee
+        pizza_orders.appendleft(pizzas_left)
 
 if not pizza_orders:
-    print(f"All orders are successfully completed!")
-    print(f"Total pizzas made: {total_pizzas}")
+    print("All orders are successfully completed!")
+    print(f"Total pizzas made: {total_pizzas_made}")
     print(f"Employees: {', '.join(str(x) for x in employees)}")
 if not employees:
     print("Not all orders are completed.")
