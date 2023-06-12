e12165a78d6be70b66ec4fbbfdc18fd0e5d267e8
Borislav Ivanov <slambeca@abv.bg>
2023-06-12 11:35:57 +0300

    Add files via upload

---
+++ Final Exams/Stacks and queues/dating_app.py
@@ -0,0 +1,90 @@
+from collections import deque
+
+males = list(map(int, input().split()))
+females = deque(map(int, input().split()))
+
+matches = 0
+
+while males and females:
+    current_female = females[0]
+    current_male = males[-1]
+
+    if current_female % 25 == 0:
+        females.popleft()
+        continue
+    elif current_male % 25 == 0:
+        males.pop()
+        continue
+
+    if current_female <= 0:
+        females.popleft()
+        continue
+    elif current_male <= 0:
+        males.pop()
+        continue
+
+    if current_female == current_male:
+        females.popleft()
+        males.pop()
+        matches += 1
+    else:
+        females.popleft()
+        current_male -= 2
+        males[-1] = current_male
+
+print(f"Matches: {matches}")
+if males:
+    males.reverse()
+    print(f"Males left: {', '.join(str(x) for x in males)}")
+else:
+    print(f"Males left: none")
+if females:
+    print(f"Females left: {', '.join(str(x) for x in females)}")
+else:
+    print(f"Females left: none")
+
+# Variant 2
+# from collections import deque
+#
+# males = [int(x) for x in input().split()]
+# females = deque([int(x) for x in input().split()])
+#
+# matches = 0
+#
+# while males and females:
+#     current_male = males[-1]
+#     current_female = females[0]
+#
+#     if current_male % 25 == 0 and current_male != 0:
+#         males.pop()
+#         males.pop()
+#         continue
+#     elif current_female % 25 == 0 and current_female != 0:
+#         females.popleft()
+#         females.popleft()
+#         continue
+#
+#     if current_male <= 0:
+#         males.pop()
+#         continue
+#     elif current_female <= 0:
+#         females.popleft()
+#         continue
+#
+#     if current_male == current_female:
+#         males.pop()
+#         females.popleft()
+#         matches += 1
+#     else:
+#         females.popleft()
+#         males[-1] -= 2
+#
+# print(f"Matches: {matches}")
+# if males:
+#     print(f"Males left: {', '.join(str(x) for x in males[::-1])}")
+# else:
+#     print("Males left: none")
+# if females:
+#     print(f"Females left: {', '.join(str(x) for x in females)}")
+# else:
+#     print("Females left: none")
