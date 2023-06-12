06537faad1a95a0ed18d2beae0a68446e47c7819
Borislav Ivanov <slambeca@abv.bg>
2023-06-12 10:46:25 +0300

    Second solution added

--- Final Exams/Stacks and queues/rubber_duck_debuggers.py
+++ Final Exams/Stacks and queues/rubber_duck_debuggers.py
@@ -32,3 +32,33 @@ while time_per_task and number_of_tasks:
 print(f"Congratulations, all tasks have been completed! Rubber ducks rewarded:")
 for key, value in dictionary.items():
     print(f"{key}: {value}")
+    
+# Variant 2
+# from collections import deque
+# 
+# times_for_a_task = deque([int(x) for x in input().split()])
+# number_of_tasks = deque([int(x) for x in input().split()])
+# 
+# darth_vader_ducky, thor_ducky, big_blue_rubber_ducky, small_yellow_rubber_ducky = 0, 0, 0, 0
+# 
+# while times_for_a_task and number_of_tasks:
+#     current_time = times_for_a_task.popleft()
+#     current_task = number_of_tasks.pop()
+# 
+#     result = current_time * current_task
+# 
+#     if 0 <= result <= 60:
+#         darth_vader_ducky += 1
+#     elif 61 <= result <= 120:
+#         thor_ducky += 1
+#     elif 121 <= result <= 180:
+#         big_blue_rubber_ducky += 1
+#     elif 181 <= result <= 240:
+#         small_yellow_rubber_ducky += 1
+#     else:
+#         times_for_a_task.append(current_time)
+#         number_of_tasks.append(current_task - 2)
+# 
+# print("Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+# print(f"Darth Vader Ducky: {darth_vader_ducky}\nThor Ducky: {thor_ducky}\nBig Blue Rubber Ducky: {big_blue_rubber_ducky}\n"
+#       f"Small Yellow Rubber Ducky: {small_yellow_rubber_ducky}")
