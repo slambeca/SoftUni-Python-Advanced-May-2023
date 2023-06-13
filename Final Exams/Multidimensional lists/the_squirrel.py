e2ee47aee1742306d9e371002f53ee525e4a4616
Borislav Ivanov <slambeca@abv.bg>
2023-06-13 16:57:07 +0300

    Second solution added

--- Final Exams/Multidimensional lists/the_squirrel.py
+++ Final Exams/Multidimensional lists/the_squirrel.py
@@ -1,4 +1,4 @@
-def check_indices(r, c, n):
+def check_indices(r, c, n):    # We check if the indexes are valid
     if 0 <= r < n and 0 <= c < n:
         return True
 
@@ -12,8 +12,13 @@ stepped_on_trap = False
 out_of_boundaries = False
 
 matrix = []
+# * * h * *
+# t * * * *
+# * h * * *
+# * h * s *
+# * * * * *
 
-directions = {
+directions = {    # Possible directions
     "up": (-1, 0),
     "down": (1, 0),
     "left": (0, -1),
@@ -21,12 +26,12 @@ directions = {
 }
 
 n = int(input())
-commands = input().split(", ")
+commands = input().split(", ")    # ['left', 'left', 'up', 'right', 'up', 'up']
 
-squirrel_position = []
+squirrel_position = []    # [2, 1]
 
 for _ in range(n):
-    matrix.append(list(input()))
+    matrix.append(list(input()))    # We use this method when there are no spaces and the input is not only numbers
 
 for row in range(n):
     for col in range(n):
@@ -59,3 +64,62 @@ for direction in commands:
 if not game_won and not stepped_on_trap and not out_of_boundaries:
     print("There are more hazelnuts to collect.")
 print(f"Hazelnuts collected: {collected_hazelnuts}")
+
+# Variant 2
+# def check_for_indices(r, c, some_array):
+#     if 0 <= r < some_array and 0 <= c < some_array:
+#         return True
+# 
+#     return False
+# 
+# 
+# collected_hazelnuts = 0
+# 
+# directions = {
+#     "up": (-1, 0),
+#     "down": (1, 0),
+#     "left": (0, -1),
+#     "right": (0, 1),
+# }
+# 
+# GOAL = 3
+# 
+# my_position = []
+# 
+# matrix = []
+# 
+# n = int(input())
+# 
+# commands = input().split(", ")
+# 
+# for row in range(n):
+#     matrix.append([x for x in input()])
+# 
+#     if "s" in matrix[row]:
+#         my_position = [row, matrix[row].index("s")]
+# 
+#         matrix[row][my_position[1]] = "*"
+# 
+# for direction in commands:
+#     wanted_row = my_position[0] + directions[direction][0]
+#     wanted_col = my_position[1] + directions[direction][1]
+# 
+#     if not check_for_indices(wanted_row, wanted_col, n):
+#         print("The squirrel is out of the field.")
+#         break
+# 
+#     if matrix[wanted_row][wanted_col] == "t":
+#         print("Unfortunately, the squirrel stepped on a trap...")
+#         break
+#     elif matrix[wanted_row][wanted_col] == "h":
+#         collected_hazelnuts += 1
+#         matrix[wanted_row][wanted_col] = "*"
+#         if collected_hazelnuts == GOAL:
+#             print("Good job! You have collected all hazelnuts!")
+#             break
+# 
+#     my_position = [wanted_row, wanted_col]
+# else:
+#     print("There are more hazelnuts to collect.")
+# 
+# print(f"Hazelnuts collected: {collected_hazelnuts}")
