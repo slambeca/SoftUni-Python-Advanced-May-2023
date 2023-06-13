2abc4a16415f05b09f26bf0686079fa07d02e96b
Borislav Ivanov <slambeca@abv.bg>
2023-06-13 13:04:51 +0300

    Fourth solution added

--- 4.3. Multidimensional Lists - Exercise/matrix_modification.py
+++ 4.3. Multidimensional Lists - Exercise/matrix_modification.py
@@ -59,3 +59,95 @@ for row in matrix:
 #     command = input().split()
 #
 # [print(*row) for row in matrix]
+
+# Variant 3
+
+
+# def check_indices(r, c, n):
+#     if 0 <= r < n and 0 <= c < n:
+#         return True
+#
+#     return False
+#
+#
+# n = int(input())
+#
+# matrix = []
+#
+# for _ in range(n):
+#     numbers = [int(x) for x in input().split()]
+#
+#     matrix.append(numbers)
+#
+# COMMAND_END = "END"
+# COMMAND_ADD = "Add"
+# COMMAND_SUBTRACT = "Subtract"
+#
+# # 1 2 3
+# # 4 5 6
+# # 7 8 9
+#
+# while True:
+#     command = input()
+#
+#     if command == COMMAND_END:
+#         break
+#
+#     command_args = command.split()
+#
+#     type_of_command = command_args[0]
+#     row = int(command_args[1])
+#     col = int(command_args[2])
+#     new_value = int(command_args[3])
+#
+#     if check_indices(row, col, n):
+#         if type_of_command == COMMAND_ADD:
+#             matrix[row][col] += new_value
+#         elif type_of_command == COMMAND_SUBTRACT:
+#             matrix[row][col] -= new_value
+#     else:
+#         print("Invalid coordinates")
+#
+# [print(*row) for row in matrix]
+
+# Variant 4
+# def check_indices(r, c, n):
+#     if 0 <= r < n and 0 <= c < n:
+#         return True
+# 
+#     return False
+# 
+# 
+# SIZE = int(input())
+# 
+# matrix = []
+# # 1 2 3
+# # 4 5 6
+# # 7 8 9
+# 
+# for row in range(SIZE):
+#     matrix.append([int(x) for x in input().split()])
+# 
+# while True:
+#     command = input()
+# 
+#     if command == "END":
+#         break
+# 
+#     command_args = command.split()
+# 
+#     type_of_action = command_args[0]
+#     row, col, value = int(command_args[1]), int(command_args[2]), int(command_args[3])
+# 
+#     if type_of_action == "Add":
+#         if check_indices(row, col, SIZE):
+#             matrix[row][col] += value
+#         else:
+#             print("Invalid coordinates")
+#     elif type_of_action == "Subtract":
+#         if check_indices(row, col, SIZE):
+#             matrix[row][col] -= value
+#         else:
+#             print("Invalid coordinates")
+# 
+# [print(*row) for row in matrix]
