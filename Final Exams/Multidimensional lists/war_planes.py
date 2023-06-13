196ca62d73cb790a286256fa9c430c8b012a2fb7
Borislav Ivanov <slambeca@abv.bg>
2023-06-13 15:43:15 +0300

    Second solution added

--- Final Exams/Multidimensional lists/war_planes.py
+++ Final Exams/Multidimensional lists/war_planes.py
@@ -29,10 +29,10 @@ for row in range(n):
     matrix.append(input().split())
 
     if "p" in matrix[row]:
-        plane_position = [row, matrix[row].index("p")]
+        plane_position = [row, matrix[row].index("p")]    # With one object, we use index
         matrix[row][plane_position[1]] = "."
     if "t" in matrix[row]:
-        all_targets += matrix[row].count("t")
+        all_targets += matrix[row].count("t")    # With multiple objects, we use count
 
 m = int(input())
 
@@ -66,3 +66,75 @@ if game_won:
 else:
     print(f"Mission failed! {all_targets - killed_targets} targets left.")
 [print(*row) for row in matrix]
+
+# Variant 2
+# def check_for_indices(r, c, some_array):
+#     if 0 <= r < some_array and 0 <= c < some_array:
+#         return True
+#
+#     return False
+#
+#
+# n = int(input())
+#
+# killed_targets = 0
+# all_targets = 0
+#
+# my_position = []
+#
+# matrix = []
+# # . . p .
+# # . . . .
+# # . . t .
+# # t . . .
+#
+# directions = {
+#     "up": (-1, 0),
+#     "down": (1, 0),
+#     "left": (0, -1),
+#     "right": (0, 1),
+# }
+#
+# for row in range(n):
+#     matrix.append(input().split())
+#
+#     if "p" in matrix[row]:
+#         my_position = [row, matrix[row].index("p")]
+#         matrix[row][my_position[1]] = "."
+#     if "t" in matrix[row]:
+#         all_targets += matrix[row].count("t")
+#
+# m = int(input())
+#
+# for _ in range(m):
+#     command = input().split()
+#
+#     type_of_command = command[0]
+#     direction = command[1]
+#     steps = int(command[2])
+#
+#     if type_of_command == "move":
+#         wanted_row = my_position[0] + directions[direction][0] * steps
+#         wanted_col = my_position[1] + directions[direction][1] * steps
+#         if check_for_indices(wanted_row, wanted_col, n):
+#             if matrix[wanted_row][wanted_col] == ".":
+#                 matrix[wanted_row][wanted_col] = "p"
+#                 matrix[my_position[0]][my_position[1]] = "."
+#                 my_position = [wanted_row, wanted_col]
+#
+#     elif type_of_command == "shoot":
+#         wanted_row = my_position[0] + directions[direction][0] * steps
+#         wanted_col = my_position[1] + directions[direction][1] * steps
+#         if check_for_indices(wanted_row, wanted_col, n):
+#             if matrix[wanted_row][wanted_col] == "t":
+#                 matrix[wanted_row][wanted_col] = "x"
+#                 killed_targets += 1
+#                 if killed_targets == all_targets:
+#                     print(f"Mission accomplished! All {all_targets} targets destroyed.")
+#                     break
+#             else:
+#                 matrix[wanted_row][wanted_col] = "x"
+# else:
+#     print(f"Mission failed! {all_targets - killed_targets} targets left.")
+#
+# [print(*row) for row in matrix]
