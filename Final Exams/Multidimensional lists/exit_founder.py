e2caad0756de9020ed438f2ab4334c172667da02
Borislav Ivanov <slambeca@abv.bg>
2023-06-14 12:14:13 +0300

    Second solution added

--- Final Exams/Multidimensional lists/exit_founder.py
+++ Final Exams/Multidimensional lists/exit_founder.py
@@ -54,3 +54,64 @@ while True:
             player_two_resting = True
 
     index += 1
+    
+# Variant 2
+# players = input().split(", ")
+# player_one, player_two = players[0], players[1]
+# 
+# matrix = []
+# # . . T . . .
+# # . . . . . .
+# # . . W . . .
+# # . . W . . E
+# # . . . . . .
+# # . T . W . .
+# 
+# index = 1
+# SIZE = 6
+# player_one_resting = False
+# player_two_resting = False
+# 
+# for row in range(SIZE):
+#     matrix.append(input().split())
+# 
+# while True:
+#     coordinates = input().split(", ")
+# 
+#     wanted_row = int(coordinates[0].strip("("))
+#     wanted_col = int(coordinates[1].strip(")"))
+# 
+#     if index % 2 != 0:
+#         if player_one_resting:
+#             player_one_resting = False
+#             index += 1
+#             continue
+#         if matrix[wanted_row][wanted_col] == "E":
+#             print(f"{player_one} found the Exit and wins the game!")
+#             raise SystemExit
+#         elif matrix[wanted_row][wanted_col] == "T":
+#             print(f"{player_one} is out of the game! The winner is {player_two}.")
+#             raise SystemExit
+#         elif matrix[wanted_row][wanted_col] == "W":
+#             index += 1
+#             print(f"{player_one} hits a wall and needs to rest.")
+#             player_one_resting = True
+#             continue
+#     else:
+#         if player_two_resting:
+#             player_two_resting = False
+#             index += 1
+#             continue
+#         if matrix[wanted_row][wanted_col] == "E":
+#             print(f"{player_two} found the Exit and wins the game!")
+#             raise SystemExit
+#         elif matrix[wanted_row][wanted_col] == "T":
+#             print(f"{player_two} is out of the game! The winner is {player_one}.")
+#             raise SystemExit
+#         elif matrix[wanted_row][wanted_col] == "W":
+#             index += 1
+#             print(f"{player_two} hits a wall and needs to rest.")
+#             player_two_resting = True
+#             continue
+# 
+#     index += 1
