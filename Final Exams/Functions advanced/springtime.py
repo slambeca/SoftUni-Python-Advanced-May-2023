d1c5cccc03d1fd8eac94b655ea7356d102dea115
Borislav Ivanov <slambeca@abv.bg>
2023-06-15 14:23:11 +0300

    Second solution added

--- Final Exams/Functions advanced/springtime.py
+++ Final Exams/Functions advanced/springtime.py
@@ -6,7 +6,7 @@ def start_spring(**kwargs):
             dictionary[value] = []
         dictionary[value].append(key)
 
-    for keys, values in dictionary.items():
+    for keys, values in dictionary.items():    # We sort the values in each list
         values.sort()
 
     sorted_dictionary = dict(sorted(dictionary.items(), key=lambda x: (-len(x[1]), x[0])))
@@ -52,3 +52,24 @@ def start_spring(**kwargs):
 #     "Butterfly": "insect"
 # }
 # print(start_spring(**example_objects))
+
+# Variant 2
+# def start_spring(**kwargs):
+#     dictionary, result = {}, ""
+# 
+#     for key, value in kwargs.items():
+#         if value not in dictionary.keys():
+#             dictionary[value] = []
+#         dictionary[value].append(key)
+# 
+#     for key, value in dictionary.items():
+#         value.sort()
+# 
+#     sorted_dict = dict(sorted(dictionary.items(), key=lambda x: (-len(x[1]), x[0])))
+# 
+#     for key, values in sorted_dict.items():
+#         result += f"{key}:\n"
+#         for value in values:
+#             result += f"-{value}\n"
+# 
+#     return result
